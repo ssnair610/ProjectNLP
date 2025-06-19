409953ce0024af16d9027aba15fce3a322557296
ssnair610 <s.shreyas2001@gmail.com>
2025-06-19 10:12:31 +0200

    Restructure

--- svm_final.py
+++ svm_final.py
@@ -19,23 +19,18 @@ def clean_text(text):
     return text.strip()
 
 def predict(csv_path):
-
     clf = joblib.load('svm_model.joblib')
     vectorizer = joblib.load('vectorizer.joblib')
-
-    
+  
     df = pd.read_csv(csv_path)
     if 'text' not in df.columns:
         raise ValueError("Input CSV must have a 'text' column.")
 
     df['clean_text'] = df['text'].fillna('').apply(clean_text)
 
-
     X_test = vectorizer.transform(df['clean_text'])
 
-
     preds = clf.predict(X_test)
-
     preds = preds.astype(int).tolist()
 
     return preds
